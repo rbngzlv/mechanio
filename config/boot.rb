0e44ba555fe58126e01d85966139cd70f4b93d4b
Anton Nikiforov <invulner91@gmail.com>
2013-09-24 19:04:45 +0300

    navbar&structure ofindex

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,6 @@
 # Set up gems listed in the Gemfile.
+require 'rubygems'
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
