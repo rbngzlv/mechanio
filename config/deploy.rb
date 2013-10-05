bd10aa0f1f9157f5292979dd5a5a0cada025b20c
Boris Bartzitz <bartzitz@gmail.com>
2013-10-05 18:00:51 +0300

    Revert "Precompile assets locally, server runs our of memory"
    This reverts commit 6076c512aa92590baff08067bb96542baddc5c26.

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,33 +43,7 @@ namespace :deploy do
   task :stop do
     run "if #{unicorn_running}; then kill -QUIT `cat #{unicorn_pid}`; fi"
   end
-
-  namespace :assets do
-    task :precompile, :roles => :web do
-      from = source.next_revision(current_revision)
-      # if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
-        run_locally("rake assets:clean && rake assets:precompile")
-        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
-        top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
-        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
-        run_locally "rm public/assets.tar.bz2"
-        run_locally("rake assets:clean")
-      # else
-      #   logger.info "Skipping asset precompilation because there were no asset changes"
-      # end
-    end
-
-    task :symlink, roles: :web do
-      run ("rm -rf #{latest_release}/public/assets &&
-            mkdir -p #{latest_release}/public &&
-            mkdir -p #{shared_path}/assets &&
-            ln -s #{shared_path}/assets #{latest_release}/public/assets")
-    end
-  end
 end
 
 before "deploy:finalize_update", "configure"
 after "deploy:update", "deploy:migrate"
-
-before 'deploy:finalize_update', 'deploy:assets:symlink'
-after 'deploy:update_code', 'deploy:assets:precompile'
