7449401bfdf44f6acca99833b621e4cc81db38b6
Boris Bartzitz <bartzitz@gmail.com>
2013-10-09 16:19:21 +0300

    Refactor wizard spec

--- spec/features/users/service_wizard_spec.rb
+++ spec/features/users/service_wizard_spec.rb
@@ -9,54 +9,53 @@ describe 'Service wizard', js: true do
   let!(:service_plan) { create :service_plan, make: make, model: model, model_variation: variation }
   let!(:state)        { create :state, name: 'State' }
 
-  context 'user has no cars/not logged in' do
+  it 'asks new user to login after Contact step' do
+    visit root_path
+    click_on 'Car needs servicing'
 
-    it 'asks new user to login after Contact step' do
-      visit root_path
-      click_on 'Car needs servicing'
+    verify_current_step 'Car Details'
+    add_new_car
 
-      verify_current_step 'Car Details'
-      add_new_car
+    verify_current_step 'Diagnose'
+    verify_sidebar 2, 'VEHICLE', variation.display_title
+    select_service_plan
 
-      verify_current_step 'Diagnose'
-      verify_sidebar 2, 'VEHICLE', variation.display_title
-      select_service_plan
+    verify_current_step 'Contact'
+    verify_sidebar 3, 'CAR SERVICING', service_plan.display_title
+    fill_in_address
 
-      verify_current_step 'Contact'
-      verify_sidebar 3, 'CAR SERVICING', service_plan.display_title
-      fill_in_address
-
-      find('#login-modal').should be_visible
-      within '#login-modal' do
-        fill_in 'user_email', with: user.email
-        fill_in 'user_password', with: user.password
-        click_on 'Login'
-      end
-
-      verify_current_step 'Quote'
-      verify_job_created
+    find('#login-modal').should be_visible
+    within '#login-modal' do
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_on 'Login'
     end
 
-    it 'lists existing user cars' do
-      car = create :car, user: user, model_variation: variation
-      login_user user
-      visit service_path
-
-      verify_current_step 'Car Details'
-      select_car(car)
-
-      verify_current_step 'Diagnose'
-      verify_sidebar 2, 'VEHICLE', variation.display_title
-      select_service_plan
-
-      verify_current_step 'Contact'
-      verify_sidebar 3, 'CAR SERVICING', service_plan.display_title
-      fill_in_address
-
-      verify_current_step 'Quote'
-      verify_job_created
-    end
+    verify_current_step 'Quote'
+    verify_job_created
   end
+
+  it 'lists existing user cars', pending: 'need shared db connection for this test to work' do
+    car = create :car, user: user, model_variation: variation
+    ap car.model_variation
+    login_user user
+    visit service_path
+
+    verify_current_step 'Car Details'
+    select_car(car)
+
+    verify_current_step 'Diagnose'
+    verify_sidebar 2, 'VEHICLE', variation.display_title
+    select_service_plan
+
+    verify_current_step 'Contact'
+    verify_sidebar 3, 'CAR SERVICING', service_plan.display_title
+    fill_in_address
+
+    verify_current_step 'Quote'
+    verify_job_created
+  end
+
 
   def add_new_car
     page.should have_css 'h5', text: 'ADD YOUR VEHICLE'
@@ -77,7 +76,7 @@ describe 'Service wizard', js: true do
     fill_in 'Street address', with: 'Broadway 54, ap. 1'
     fill_in 'Suburb', with: 'Suburb'
     fill_in 'Postcode', with: '1234'
-    select  'State', from: 'location_state_id'
+    select  state.name, from: 'location_state_id'
     fill_in 'job_contact_email', with: 'email@host.com'
     fill_in 'job_contact_phone', with: '123 456789'
     click_on 'Continue'
@@ -101,7 +100,7 @@ describe 'Service wizard', js: true do
   end
 
   def verify_job_created
-    user.jobs.count.should eq 1
+    user.reload.jobs.count.should eq 1
     user.jobs.last.status.should eq 'pending'
   end
 
