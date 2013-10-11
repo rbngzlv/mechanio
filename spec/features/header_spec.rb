637e359ad47123bb7edf6accac9850ad84688ba6
Eugene Maslenkov <maslenkov@kodolabs.com>
2013-10-11 16:59:35 +0300

    add real link to header and test it

---
+++ spec/features/header_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+feature 'navigation in header' do
+  scenario 'user' do
+    login_user
+    visit users_dashboard_path
+    within 'header' do
+      click_link 'My Estimates'
+      current_path.should be_eql users_estimates_path
+      click_link 'My Profile'
+      current_path.should be_eql users_profile_path
+      click_link 'Dashboard'
+      current_path.should be_eql users_dashboard_path
+      click_link 'Log out'
+      current_path.should be_eql root_path
+    end
+  end
+
+  scenario 'mechanic' do
+    login_mechanic
+    visit mechanics_dashboard_path
+    within 'header' do
+      click_link 'My Profile'
+      current_path.should be_eql mechanics_profile_path
+      click_link 'Dashboard'
+      current_path.should be_eql mechanics_dashboard_path
+      click_link 'Log out'
+      current_path.should be_eql root_path
+    end
+  end
+end
