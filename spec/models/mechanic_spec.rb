ec03c6277ae494bb9e11701d3d9342c90bcb8cb4
Eugene Maslenkov <maslenkov@kodolabs.com>
2013-10-25 17:42:18 +0300

    mechanics ordering by locations(version 0.3)

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -17,4 +17,17 @@ describe Mechanic do
   it { should respond_to :driver_license }
   it { should respond_to :abn }
   it { should respond_to :mechanic_license }
+
+  describe 'scope close_to' do
+    let(:location1) { create :location, :with_type, latitude: 40.000000, longitude: -77.000000 }
+    let(:location2) { create :location, :with_type, latitude: 39.010000, longitude: -75.990000 }
+    let(:location3) { create :location, :with_type, latitude: 40.010000, longitude: -78.000000 }
+    let!(:mechanic1) { create :mechanic, location: location1 }
+    let!(:mechanic2) { create :mechanic, location: location2 }
+    let!(:mechanic3) { create :mechanic, location: location3 }
+
+    it 'should sort mechanics by distance' do
+      Mechanic.close_to(39.000000, -76.000000).should == [mechanic2, mechanic1, mechanic3]
+    end
+  end
 end
