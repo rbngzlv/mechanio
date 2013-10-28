97fdb171b24f7ed7f15cf3eaa829a85fc8f001b9
Eugene Maslenkov <maslenkov@kodolabs.com>
2013-10-28 16:36:29 +0200

    improve database clianers config
    for saving postgis helping tables btw starting tests

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -2,7 +2,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.before :each do
-    DatabaseCleaner.strategy = example.metadata[:js] ? :deletion : :transaction
+    DatabaseCleaner.strategy = example.metadata[:js] ? :deletion : :truncation, { except: %w[spatial_ref_sys] }
     DatabaseCleaner.start
   end
 
