9afd9416cd4e5465f18e9dea8f569a39f563ac6d
Anton Nikiforov <invulner91@gmail.com>
2013-12-17 17:10:09 +0200

    Responsive sidebar

--- app/assets/javascripts/mechanio.js
+++ app/assets/javascripts/mechanio.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
   // customize bootstrap file_upload_plugin
   $('input[type=file]').bootstrapFileInput();
   $('.file-input-wrapper').addClass('btn-success col-md-3');
+
+  $('.mobile-nav').click(function(){
+    $('.aside').wrap('<div class = "aside-wrapper"></div>').addClass('show-aside');
+    $('.header').css('z-index', '1');
+
+    $('.aside-wrapper').click(function(){
+      $('.aside').unwrap().removeClass('show-aside');
+    });
+  })
 });
