0c23c1697ea3d5c21bd51ad2cec20ec734f73499
Eugene Maslenkov <maslenkov@kodolabs.com>
2013-12-27 11:38:36 +0200

    add fields in admin michanic index page
    Completed job count
    fake reviews count
    phone number
    empty field for mechanic earnings
    
    and test, but it is not working because earning is empty in this commit

--- spec/features/admins/mechanics_spec.rb
+++ spec/features/admins/mechanics_spec.rb
@@ -9,9 +9,36 @@ feature 'Admin mechanics management' do
   end
 
   it 'lists available mechanics' do
+    # TODO: вынести это в лет?
+    mechanic.mobile_number = '0410123456'
+    # TODO: this string need for update phone number
+    mechanic.save
+    create(:job_with_service, :completed, mechanic: mechanic)
+    create(:job_with_service, :confirmed, mechanic: mechanic)
+    create(:job_with_service, :assigned, mechanic: mechanic)
     visit admins_mechanics_path
 
     page.should have_css 'td', text: mechanic.full_name
+# 2) Missing Table headers:
+# 2.1) Completed Jobs: (able sort through most – least) amount of jobs completed in columns
+# 2.2) Total Earnings (can sort through highest earning – lowest earning): $ amount in columns
+# 2.3) Average Feedback Score: (can sort through highest score – lowest score) Score between 0 to 5 in columns
+# 2.4) Mobile Number: Mechanic Mobile number displayed in columns
+    page.should have_css 'th:nth-child(2)', text: 'Completed Jobs'
+    page.should have_css 'th:nth-child(3)', text: 'Total Earnings'
+    page.should have_css 'th:nth-child(4)', text: 'Average Feedback Score'
+    page.should have_css 'th:nth-child(5)', text: 'Mobile Number'
+    within 'tbody' do
+      # также здесь нужно разобраться - какие именно работы считаются комплитед(оплаченные с фидбеком/без, не оплаченные?)
+      # TODO: я не могу проверить первый пункт так как не известно, что в данном случае является закрытм таском...
+      page.should have_css 'tr:nth-child(1) td:nth-child(2)', text: '1' # я должен быть уверенным, что здесь нужное мне значение(например две законченных работы)
+      page.should have_css 'tr:nth-child(1) td:nth-child(3)', text: mechanic.earnings # такого метода нет, он должен верно подсчитывать кол-во денег, соответственно на него нужен "тест вперед"
+      # может покачто earnings тоже заткнуть фейковым методом?
+      page.should have_css 'tr:nth-child(1) td:nth-child(4)', text: '2' # есть фейковый метод mechanic.rating, его нужно превратить в реальный и добавить к нему тесты(а лучше его за БДДшить)
+      page.should have_css 'tr:nth-child(1) td:nth-child(5)', text: '0410123456' # я должен быть уверен в значениях которые должны тобораэаться, т.е. вместо образения к свойству механика
+      # здесь должен быть номер телефона, который у механика(на самом деле тоже самое косается и всех остальных пунктов), т.е. получается, что сейчас я накидал способы получения значений
+      # для данных полей, а в реальном тесте должны быть статические строки
+    end
   end
 
   it 'shows mechanic details' do
