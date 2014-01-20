5b021a32b78c3a12e91a2f0bfaed214c1978c210
Boris Bartzitz <bartzitz@gmail.com>
2014-01-20 22:37:08 +0200

    Update admin edit mechanic and mechanic profile forms

--- spec/features/admins/mechanics_spec.rb
+++ spec/features/admins/mechanics_spec.rb
@@ -2,30 +2,21 @@ require 'spec_helper'
 
 feature 'Admin mechanics management' do
 
-  let!(:mechanic) { create :mechanic }
+  let(:mechanic) { create :mechanic, mobile_number: '0410123456' }
+  let(:next_year) { (Date.today.year + 1).to_s }
+  let!(:state) { create :state, name: 'QLD' } # FIXME: states should be preloaded form seeds before suite
 
   before do
     login_admin
   end
 
   it 'lists available mechanics' do
-    mechanic.mobile_number = '0410123456'
-    mechanic.save
-    create(:job_with_service, :completed, mechanic: mechanic)
-    create(:job_with_service, :confirmed, mechanic: mechanic)
-    create(:job_with_service, :assigned, mechanic: mechanic)
+    mechanic
     visit admins_mechanics_path
 
-    page.should have_css 'td', text: mechanic.full_name
-    page.should have_css 'th:nth-child(3)', text: 'Total Earnings'
-    page.should have_css 'th:nth-child(4)', text: 'Average Feedback Score'
-    page.should have_css 'th:nth-child(5)', text: 'Mobile Number'
-    within 'tbody' do
-      page.should have_css 'tr:nth-child(1) td:nth-child(2)', text: '0'
-      page.should have_css 'tr:nth-child(1) td:nth-child(3)', text: '0$'
-      page.should have_css 'tr:nth-child(1) td:nth-child(4)', text: '2'
-      page.should have_css 'tr:nth-child(1) td:nth-child(5)', text: '0410123456'
-    end
+    date_joined = mechanic.created_at.to_s(:date_short)
+    page.should have_css 'thead', text: 'Name Email Mobile Current Appts. Completed Jobs Total Earnings Feedback Score Date joined'
+    page.should have_css 'tbody tr', text: "#{mechanic.full_name} #{mechanic.email} 0410123456 0 0 $0.00 2 #{date_joined} Edit"
   end
 
   it 'shows mechanic details' do
@@ -35,7 +26,7 @@ feature 'Admin mechanics management' do
     page.should have_field 'First name', with: mechanic.first_name
   end
 
-  context 'adds a new mechanic' do
+  context 'adds a new mechanic', :js do
     scenario 'success' do
       mail_deliveries.clear
       visit admins_mechanics_path
@@ -43,23 +34,54 @@ feature 'Admin mechanics management' do
 
       expect do
         fill_in 'First name', with: 'First'
-        fill_in 'Last name', with: 'Last'
-        fill_in 'Email Address', with: 'mechanic@host.com'
-        select '1975',    from: 'mechanic_dob_1i'
-        select 'October', from: 'mechanic_dob_2i'
-        select '1',       from: 'mechanic_dob_3i'
+        fill_in 'Last name',  with: 'Last'
+        fill_in 'Email',      with: 'mechanic@host.com'
+        select  '1975',       from: 'mechanic_dob_1i'
+        select  'October',    from: 'mechanic_dob_2i'
+        select  '1',          from: 'mechanic_dob_3i'
         fill_in 'Personal description', with: 'Something about me'
-        fill_in 'mechanic_location_attributes_address', with: 'Seashell avenue, 25'
-        fill_in 'mechanic_location_attributes_suburb', with: 'Somewhere'
-        select 'Queensland', from: 'mechanic_location_attributes_state_id'
-        fill_in 'mechanic_location_attributes_postcode', with: 'AX12345'
-        fill_in 'Mobile number', with: '0410123456'
-        fill_in "License number", with: 'MXF123887364'
-        select 'Queensland', from: "mechanic_license_state_id"
-        select '2015',      from: 'mechanic_license_expiry_1i'
-        select 'September', from: 'mechanic_license_expiry_2i'
-        select '1',         from: 'mechanic_license_expiry_3i'
+        fill_in 'Years as mechanic',    with: '3'
+
+        click_on 'Contact details'
+        fill_in 'Mobile number',  with: '0410123456'
+        fill_in 'Street Address', with: 'Seashell avenue, 25'
+        fill_in 'Suburb',         with: 'Somewhere'
+        select  'QLD',            from: 'State'
+        fill_in 'Postcode',       with: 'AX12345'
+
+        click_on 'Business details'
+        fill_in 'Business name', with: 'My company'
+        fill_in 'ABN number',    with: '12345678900'
+        select  next_year,       from: 'mechanic_abn_expiry_1i'
+        select  'October',       from: 'mechanic_abn_expiry_2i'
+        select  '1',             from: 'mechanic_abn_expiry_3i'
+        fill_in 'Business website', with: 'http://my-company.com'
+        fill_in 'Business email',   with: 'email@company.com'
+        fill_in 'Business phone number', with: '01412345678'
+        fill_in 'Business address', with: 'Sunset Blvd. 26'
+        fill_in 'Suburb',           with: 'Sydney'
+        select  'QLD',              from: 'State'
+        fill_in 'Postcode',         with: '2000'
+
+        click_on 'Driver license'
+        fill_in 'License number', with: 'MXF12388736423887364'
+        select  'QLD',        from: 'Registered state'
+        select  next_year,    from: 'mechanic_license_expiry_1i'
+        select  'September',  from: 'mechanic_license_expiry_2i'
+        select  '1',          from: 'mechanic_license_expiry_3i'
+
+        click_on 'Motor license'
+        fill_in 'Motor license number', with: 'MXF12388736423887364'
+        select  'QLD',        from: 'Registered state'
+        select  next_year,    from: 'mechanic_mechanic_license_expiry_1i'
+        select  'September',  from: 'mechanic_mechanic_license_expiry_2i'
+        select  '1',          from: 'mechanic_mechanic_license_expiry_3i'
+
+        click_on 'Badges'
+        check 'Phone verified'
+
         click_button 'Save'
+
       end.to change { Mechanic.count }.by 1
 
       page.should have_css '.alert', text: 'Mechanic successfully created.'
@@ -70,33 +92,30 @@ feature 'Admin mechanics management' do
     scenario 'fail' do
       visit admins_mechanics_path
       click_link 'Add mechanic'
-      page.should have_selector "h4", text: 'Add mechanic'
-      page.should have_selector "legend", text: "Mechanic Details"
+
+      click_on 'Contact details'
       within '.mechanic_location_address' do
         page.should have_selector 'abbr', text: '*'
       end
+
+      click_on 'Business details'
       within '.mechanic_business_location_address' do
         page.should have_no_selector 'abbr', text: '*'
       end
       click_button 'Save'
-      page.should have_content "Please review the problems below:"
-      page.should have_selector "#mechanic_business_location_attributes_address"
+
+      page.should have_content 'Please review the problems below:'
     end
   end
 
   context 'edits existing mechanic' do
+    before { mechanic }
+
     scenario 'success' do
       visit admins_mechanics_path
       page.should have_css 'td', text: 'Joe Mechanic'
 
       click_link 'Edit'
-      page.should have_content 'Business Details'
-      page.should have_content 'Verified Statuses'
-      check 'Phone verified'
-      check 'Super mechanic'
-      check 'Warranty covered'
-      check 'Qualification verified'
-
       fill_in 'First name', with: 'Alex'
       click_button 'Save'
 
@@ -115,30 +134,36 @@ feature 'Admin mechanics management' do
     end
   end
 
-  it 'deletes a mechanic' do
-    expect do
-      visit edit_admins_mechanic_path(mechanic)
-      click_link 'Delete'
-    end.to change { Mechanic.count }.by -1
+  it 'deletes a mechanic', :js do
+    visit edit_admins_mechanic_path(mechanic)
 
+    expect { click_link 'Delete' }.to change { Mechanic.count }.by -1
     page.should have_css '.alert', text: 'Mechanic successfully deleted.'
   end
 
   context 'edit images' do
     let(:image_path) { "#{Rails.root}/spec/features/fixtures/test_img.jpg" }
 
-    scenario "uploading" do
+    scenario 'uploading' do
       visit edit_admins_mechanic_path(mechanic)
       attach_file('mechanic_avatar', image_path)
-      attach_file('mechanic_driver_license', image_path)
+
+      click_on 'Business details'
       attach_file('mechanic_abn', image_path)
+
+      click_on 'Driver license'
+      attach_file('mechanic_driver_license', image_path)
+
+      click_on 'Motor license'
       attach_file('mechanic_mechanic_license', image_path)
+
       expect { click_button 'Save' }.to change { all('img').length }.by(4)
+
       page.should have_content 'Mechanic successfully updated.'
-      page.should have_selector 'img + div', text: 'test_img.jpg', count: 4
+      page.should have_selector 'img + div', text: 'test_img.jpg'
     end
 
-    scenario "deleting", :js do
+    scenario 'deleting', :js do
       mechanic.avatar = File.open(image_path)
       mechanic.save
       visit edit_admins_mechanic_path(mechanic)
@@ -151,8 +176,9 @@ feature 'Admin mechanics management' do
       page.should have_content 'Image successfully deleted.'
     end
 
-    scenario "uploading cancel", :js do
+    scenario 'uploading cancel', :js do
       visit edit_admins_mechanic_path(mechanic)
+      click_on 'Motor license'
       attach_file('mechanic_mechanic_license', image_path)
 
       page.should have_selector '.file-input-name', text: 'test_img.jpg'
--- spec/features/mechanics/edit_profile_spec.rb
+++ spec/features/mechanics/edit_profile_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'mechanic edit profile page' do
-  let(:mechanic) { create :mechanic }
+  let(:mechanic) { create :mechanic, first_name: 'Bob' }
 
   subject { page }
 
@@ -13,38 +13,25 @@ feature 'mechanic edit profile page' do
   context 'edit' do
     scenario "fail" do
       fill_in 'mechanic_first_name', with: ''
-      click_button "Save"
+      click_button 'Save'
 
       should have_content 'Please review the problems below'
       should have_selector '.has-error', text: "can't be blank"
     end
 
     scenario 'success' do
-      new_mechanic_name = "#{mechanic.first_name}_new_name"
-      fill_in 'mechanic_first_name', with: new_mechanic_name
-      click_button "Save"
+      fill_in 'First name', with: 'Alex'
+      click_button 'Save'
+
       should have_selector '.alert.alert-info', text: 'Your profile successfully updated.'
     end
 
-    scenario "upload avatar" do
+    scenario 'upload avatar' do
       attach_file('mechanic_avatar', "#{Rails.root}/spec/features/fixtures/test_img.jpg")
       click_button 'Save'
 
       should have_content 'Your profile successfully updated.'
-      find('img.avatar')['src'].should have_content mechanic.reload.avatar_url :thumb
+      find('.mechanic_avatar img')['src'].should have_content mechanic.reload.avatar_url :thumb
     end
-  end
-
-  scenario 'check content' do
-    should have_selector 'select:nth-child(1)#mechanic_dob_3i'
-    should have_selector 'select:nth-child(2)#mechanic_dob_2i'
-    should have_selector 'select:nth-child(3)#mechanic_dob_1i'
-
-    should have_selector 'label', text: 'Driver License Number'
-
-    should have_selector 'input[type=text]#mechanic_years_as_a_mechanic'
-
-    click_link 'Availabilities'
-    should have_selector 'h1', text: 'Calendar'
   end
 end
