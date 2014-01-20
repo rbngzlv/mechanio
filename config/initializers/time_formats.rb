b041592dfdc0c5f2e8665dad74fca60fe40b6223
Boris Bartzitz <bartzitz@gmail.com>
2014-01-20 17:03:31 +0200

    Fix mechanics admin index

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,5 +1,6 @@
 Time::DATE_FORMATS[:month_year] = "%b %Y"
 Time::DATE_FORMATS[:date] = "%d %b %Y (%A)"
+Time::DATE_FORMATS[:date_short] = "%d %b %Y"
 Time::DATE_FORMATS[:date_time] = "%I:%M %p %d %b %Y (%A)"
 Time::DATE_FORMATS[:date_time_short] = "%I:%M %p %d %b %Y"
 Time::DATE_FORMATS[:time_day_month] = "%l %p %d %b"
