0b7e4e0e863eeb7dc22d49c706af4f6a466457c9
Boris Bartzitz <bartzitz@gmail.com>
2014-01-20 10:13:23 +0200

    Remove "confirmed" status from Job model

--- spec/models/jobs/job_spec.rb
+++ spec/models/jobs/job_spec.rb
@@ -57,40 +57,41 @@ describe Job do
   end
 
   describe '#assign_mechanic' do
+    let(:job) { create :job, :with_service, :with_credit_card }
     let(:mechanic) { create :mechanic }
 
     it 'return true if success' do
       Job.any_instance.should_receive(:notify_assigned)
-      job_with_service.assign_mechanic(mechanic_id: mechanic.id, scheduled_at: DateTime.tomorrow).should be_true
+      job.assign_mechanic(mechanic_id: mechanic.id, scheduled_at: DateTime.tomorrow).should be_true
     end
 
     it 'return false if sheduled time doesnot given' do
       Job.any_instance.should_not_receive(:notify_assigned)
-      job_with_service.assign_mechanic(mechanic_id: mechanic.id).should be_false
+      job.assign_mechanic(mechanic_id: mechanic.id).should be_false
     end
 
     it 'return false if sheduled time in the past' do
       Job.any_instance.should_not_receive(:notify_assigned)
-      job_with_service.assign_mechanic(mechanic_id: mechanic.id, scheduled_at: Date.yesterday).should be_false
+      job.assign_mechanic(mechanic_id: mechanic.id, scheduled_at: Date.yesterday).should be_false
     end
 
     it 'return false if mechanic unavailable in choosen time' do
       Job.any_instance.should_not_receive(:notify_assigned)
       create :event, mechanic: mechanic, date_start: Date.tomorrow
-      job_with_service.assign_mechanic(mechanic_id: mechanic.id, scheduled_at: DateTime.tomorrow + 11.hour).should be_false
+      job.assign_mechanic(mechanic_id: mechanic.id, scheduled_at: DateTime.tomorrow + 11.hour).should be_false
     end
 
     it 'throw exception if mechanic doesnot given' do
       Job.any_instance.should_not_receive(:notify_assigned)
       expect do
-        job_with_service.assign_mechanic(scheduled_at: DateTime.now)
+        job.assign_mechanic(scheduled_at: DateTime.now)
       end.to raise_error
     end
 
     it 'throw exception if mechanic doesnot presence' do
       Job.any_instance.should_not_receive(:notify_assigned)
       expect do
-        job_with_service.assign_mechanic(mechanic_id: 10000, scheduled_at: DateTime.now)
+        job.assign_mechanic(mechanic_id: 10000, scheduled_at: DateTime.now)
       end.to raise_error
     end
   end
