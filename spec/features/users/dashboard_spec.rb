cf57c828e22433b0d2f4b5f00a435279b3ab0170
Boris Bartzitz <bartzitz@gmail.com>
2014-01-20 10:47:08 +0200

    Fix verified icons

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -10,6 +10,16 @@ feature 'dashboard page' do
     visit users_dashboard_path
   end
 
+  it 'shows verified icons' do
+    within '.verified-icons' do
+      pending 'User mobile and payment method verifications are not implemented yet'
+
+      find('i:nth-child(1)')[:class].should eq ''
+      find('i:nth-child(2)')[:class].should eq 'icon-mobile-phone'
+      find('i:nth-child(3)')[:class].should eq ''
+    end
+  end
+
   context 'should have dynamic content' do
     include_examples("description block") do
       let(:reviews_count) { "Reviews Left: #{user.reviews}" }
