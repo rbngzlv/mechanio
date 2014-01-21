f7284823eba811702327ccc66c58424998f067ad
Eugene Maslenkov <maslenkov@kodolabs.com>
2014-01-21 15:00:15 +0200

    udpate edit mechanic feature spec
    create feature/admins/mechanic_create_spec
    udpate feature/mechanics/edit_profile_spec

--- app/controllers/mechanics/profiles_controller.rb
+++ app/controllers/mechanics/profiles_controller.rb
@@ -20,11 +20,10 @@ class Mechanics::ProfilesController < Mechanics::ApplicationController
   def permitted_params
     params.require(:mechanic).permit(
       :first_name, :last_name, :email, :dob, :description,
-      :driver_license_number, :license_state_id, :license_expiry, :avatar,
-      :driver_license, :abn, :mechanic_license, :abn_name,
-      :business_website, :business_email, :years_as_a_mechanic, :mobile_number,
-      :other_number, :abn_number, :abn_expiry, :mechanic_license_number,
-      :mechanic_license_expiry, :mechanic_license_state_id
+      :avatar, :mobile_number, :business_name, :abn_number, :abn_expiry,
+      :business_website, :business_email, :business_mobile_number, :abn,
+      location_attributes: [:address, :suburb, :postcode, :state_id],
+      business_location_attributes: [:address, :suburb, :postcode, :state_id]
     )
   end
 end
--- spec/features/mechanics/edit_profile_spec.rb
+++ spec/features/mechanics/edit_profile_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 feature 'mechanic edit profile page' do
   let(:mechanic) { create :mechanic, first_name: 'Bob' }
+  let(:next_year)  { (Date.today.year + 1).to_s }
+  let(:image_path) { "#{Rails.root}/spec/features/fixtures/test_img.jpg" }
+  let!(:state) { create :state, name: 'QLD' } # FIXME: states should be preloaded form seeds before suite
 
   subject { page }
 
@@ -11,19 +14,82 @@ feature 'mechanic edit profile page' do
   end
 
   context 'edit' do
-    scenario "fail" do
+    scenario 'success 2', :js do
+      fill_in 'First name',     with: 'First'
+      fill_in 'Last name',      with: 'Last'
+      fill_in 'Personal email', with: 'new_email@host.com'
+      select  '1975',           from: 'mechanic_dob_1i'
+      select  'October',        from: 'mechanic_dob_2i'
+      select  '1',              from: 'mechanic_dob_3i'
+      fill_in 'Personal description', with: 'Something about me'
+      attach_file 'mechanic_avatar',  image_path
+
+      click_on 'Contact details'
+      fill_in 'Mobile number',  with: '0410123456'
+      fill_in 'Street Address', with: 'Seashell avenue, 25'
+      fill_in 'Suburb',         with: 'Somewhere'
+      select  'QLD',            from: 'State'
+      fill_in 'Postcode',       with: 'AX12345'
+
+      click_on 'Business details'
+      fill_in 'Business name', with: 'My company'
+      fill_in 'ABN number',    with: '12345678900'
+      select  next_year,       from: 'mechanic_abn_expiry_1i'
+      select  'October',       from: 'mechanic_abn_expiry_2i'
+      select  '1',             from: 'mechanic_abn_expiry_3i'
+      fill_in 'Business website', with: 'http://my-company.com'
+      fill_in 'Business email',   with: 'email@company.com'
+      fill_in 'Business phone number', with: '01412345678'
+      fill_in 'Business address', with: 'Sunset Blvd. 26'
+      fill_in 'Suburb',           with: 'Sydney'
+      select  'QLD',              from: 'State'
+      fill_in 'Postcode',         with: '2000'
+      attach_file 'mechanic_abn', image_path
+
+      click_button 'Save'
+
+      ####################################
+
+      page.should have_css '.alert', text: 'Your profile successfully updated.'
+
+      page.should have_field  'First name', with: 'First'
+      page.should have_field  'Last name',  with: 'Last'
+      page.should have_field  'Personal email',      with: 'new_email@host.com'
+      page.should have_field  'mechanic_dob_1i', with:     '1975'
+      page.should have_select 'mechanic_dob_2i', selected: 'October'
+      page.should have_field  'mechanic_dob_3i', with:     '1'
+      page.should have_field  'Personal description', with: 'Something about me'
+      page.find('.mechanic_avatar img')['src'].should have_content 'test_img.jpg'
+
+      click_on 'Contact details'
+      page.should have_field  'Mobile number',  with: '0410123456'
+      page.should have_field  'Street Address', with: 'Seashell avenue, 25'
+      page.should have_field  'Suburb',         with: 'Somewhere'
+      page.should have_select 'mechanic_location_attributes_state_id', selected: 'QLD'
+      page.should have_field  'Postcode',       with: 'AX12345'
+
+      click_on 'Business details'
+      page.should have_field  'Business name', with: 'My company'
+      page.should have_field  'ABN number',    with: '12345678900'
+      page.should have_field  'mechanic_abn_expiry_1i', with: next_year
+      page.should have_select 'mechanic_abn_expiry_2i', selected: 'October'
+      page.should have_field  'mechanic_abn_expiry_3i', with: '1'
+      page.should have_field  'Business website',      with: 'http://my-company.com'
+      page.should have_field  'Business email',        with: 'email@company.com'
+      page.should have_field  'Business phone number', with: '01412345678'
+      page.should have_field  'Business address',      with: 'Sunset Blvd. 26'
+      page.should have_field  'Suburb',                with: 'Sydney'
+      page.should have_select 'mechanic_business_location_attributes_state_id', selected: 'QLD'
+      page.should have_field  'Postcode',              with: '2000'
+      page.should have_selector 'img + div', text: 'test_img.jpg'
+    end
+
+    scenario 'fail' do
       fill_in 'mechanic_first_name', with: ''
       click_button 'Save'
 
       should have_content 'Please review the problems below'
       should have_selector '.has-error', text: "can't be blank"
-    end
-
-    scenario 'success' do
-      fill_in 'First name', with: 'Alex'
-      click_button 'Save'
-
-      should have_selector '.alert.alert-info', text: 'Your profile successfully updated.'
     end
 
     scenario 'upload avatar' do
