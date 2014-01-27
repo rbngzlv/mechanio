a46f83dd77d622ad973e33306fd19fb2e0d1d01e
Eugene Maslenkov <maslenkov@kodolabs.com>
2014-01-27 17:52:39 +0200

    fix messages for social connections feature

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,22 +11,30 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
 
   def auth_with(provider)
-    auth = request.env["omniauth.auth"]
+    auth = request.env['omniauth.auth']
     if @auth = Authentication.find_or_create_from_oauth(auth, current_user)
       if @auth.user.persisted?
         if user_signed_in?
-          flash[:success] = 'Successfully added'
+          set_flash_message(:success, :add_connection, kind: kind_human(provider))
           redirect_to edit_users_profile_path(anchor: 'social-connections')
         else
-          set_flash_message(:notice, :success, kind: provider.capitalize)
+          set_flash_message(:notice, :success, kind: kind_human(provider))
           sign_in_and_redirect @auth.user, event: :authentication
         end
       else
-        session["devise.oauth_data"] = auth
+        session['devise.oauth_data'] = auth
         redirect_to new_user_registration_url, alert: 'Error occurred'
       end
     else
-      redirect_to edit_users_profile_path(anchor: 'social-connections'), alert: 'Connection not available'
+      set_flash_message(:alert, :already_connected, kind: kind_human(provider))
+      redirect_to edit_users_profile_path(anchor: 'social-connections')
+    end
+  end
+
+  def kind_human(provider)
+    case provider
+      when 'google_oauth2' then 'Gmail'
+      else provider.capitalize
     end
   end
 end
