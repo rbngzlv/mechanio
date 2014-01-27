2915c85602329c5e0b2fe4411740fcfa581e3a76
Eugene Maslenkov <maslenkov@kodolabs.com>
2014-01-27 16:26:08 +0200

    drop omniauth callbacks controoler spec

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe Users::OmniauthCallbacksController do
-  it do
-    get user_omniauth_authorize_path(:facebook)
-    response.should redirect_to 'http://facebook'
-  end
-end
