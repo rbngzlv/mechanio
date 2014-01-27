0d5fc4e93ab2b9bfd9235aebe0f7c3da7b9b8ce3
Eugene Maslenkov <maslenkov@kodolabs.com>
2014-01-27 17:24:39 +0200

    update adding social connections feature spec

--- app/controllers/users/authentications_controller.rb
+++ app/controllers/users/authentications_controller.rb
@@ -1,6 +1,6 @@
 class Users::AuthenticationsController < Users::ApplicationController
   def destroy
-    current_user.authentications.find_by_id(params[:id]).destroy
+    current_user.authentications.find(params[:id]).destroy
     redirect_to edit_users_profile_path(anchor: 'social-connections'), notice: 'Connection successfully destroyed'
   end
 end
