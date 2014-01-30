02e7824c5e4a25ff7a55fdc22ca1a433f226c70c
Eugene Maslenkov <maslenkov@kodolabs.com>
2014-01-30 16:23:06 +0200

    code improvements for new credit card page
    added integration test for added fields

--- spec/features/users/payment_page_spec.rb
+++ spec/features/users/payment_page_spec.rb
@@ -2,22 +2,34 @@ require 'spec_helper'
 
 feature 'payment page spec' do
   let(:user)     { create :user }
-  let(:mechanic) { create :mechanic, first_name: 'First', last_name: 'Last',
-    years_as_a_mechanic: 2 }
-  let!(:job)     { create :assigned_job, user: user, mechanic: mechanic }
+  let(:mechanic) { create :mechanic, years_as_a_mechanic: 2 }
+  let!(:job)     { create :assigned_job, user: user, mechanic: mechanic, scheduled_at: DateTime.new(2014, 2, 6, 11)}
 
-  before { login_user user }
-
-  it do
+  before do
+    login_user user
     visit new_users_job_credit_card_path(job)
-    # save_and_open_page
-    page.should have_css 'h4', text: '1,000 kms / 6 months service'
-    page.should have_css 'tr:nth-child(1)', text: 'First Last 2 years experience'
-    page.should have_css 'tr:nth-child(3)', text: 'Jan 29, 2014(Monday) 17:00'
-    page.should have_css 'tr:nth-child(4)', text: 'display car name'
-    page.should have_css 'tr:nth-child(5)', text: 'location'
-    # price break down
-    # button text
-    # mechanic POPUP
+  end
+
+  specify 'check content', :js do
+    within('section > .panel:nth-of-type(1)') do
+      page.should have_css 'h4', text: job.title
+      page.should have_css 'tr:nth-child(1)', text: mechanic.full_name
+      page.should have_css 'tr:nth-child(1)', text: '2 years of experience'
+      page.should have_css 'tr:nth-child(3)', text: '06 Feb 2014 (Thursday) 11:00'
+      page.should have_css 'tr:nth-child(4)', text: job.car.display_title
+      page.should have_css 'tr:nth-child(5)', text: job.location.full_address
+    end
+    within('section > .panel:nth-of-type(2)') do
+      page.should have_css 'h4', text: 'Price Breakdown'
+      page.should have_css 'tr:nth-child(1)', text: job.tasks.first.title
+      page.should have_css 'tr:nth-child(1)', text: job.tasks.first.title
+      page.should have_css 'tr:nth-child(3)', text: job.cost
+      page.should have_link 'Redeem promo code'
+      page.should have_content 'You pay $0 to secure this booking.'
+      page.should have_content 'You pay after the job is done.'
+    end
+    page.should have_button "Book #{mechanic.first_name} for Thu 6/2"
+    find('.profile-border.clickable').click
+    page.should have_css "#js-mechanic-#{mechanic.id}", visible: true
   end
 end
