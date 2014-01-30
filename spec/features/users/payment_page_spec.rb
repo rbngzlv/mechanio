5c2cff9271132b1b1acc8c0bbc2fce9ace211ea3
Eugene Maslenkov <maslenkov@kodolabs.com>
2014-01-30 17:55:37 +0200

    update code style

--- spec/features/users/payment_page_spec.rb
+++ spec/features/users/payment_page_spec.rb
@@ -10,7 +10,7 @@ feature 'payment page spec' do
     visit new_users_job_credit_card_path(job)
   end
 
-  specify 'check content', :js do
+  it 'shows job details' do
     within('section > .panel:nth-of-type(1)') do
       page.should have_css 'h4', text: job.title
       page.should have_css 'tr:nth-child(1)', text: mechanic.full_name
@@ -19,15 +19,21 @@ feature 'payment page spec' do
       page.should have_css 'tr:nth-child(4)', text: job.car.display_title
       page.should have_css 'tr:nth-child(5)', text: job.location.full_address
     end
-    within('section > .panel:nth-of-type(2)') do
+  end
+
+  it 'shows job quote' do
+    within('section > .panel:nth-of-type(3)') do
       page.should have_css 'h4', text: 'Price Breakdown'
       page.should have_css 'tr:nth-child(1)', text: job.tasks.first.title
       page.should have_css 'tr:nth-child(1)', text: job.tasks.first.title
       page.should have_css 'tr:nth-child(3)', text: job.cost
-      page.should have_link 'Redeem promo code'
+      # page.should have_link 'Redeem promo code'
       page.should have_content 'You pay $0 to secure this booking.'
       page.should have_content 'You pay after the job is done.'
     end
+  end
+
+  it 'should show mechanic details by clicking on profile block', :js do
     page.should have_button "Book #{mechanic.first_name} for Thu 6/2"
     find('.profile-border.clickable').click
     page.should have_css "#js-mechanic-#{mechanic.id}", visible: true
