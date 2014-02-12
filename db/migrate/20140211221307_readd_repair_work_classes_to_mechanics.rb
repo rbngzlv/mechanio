7744027e57aea293b7d0c22d7e7dce22b74a0cf4
Boris Bartzitz <bartzitz@gmail.com>
2014-02-12 10:09:19 +0200

    Readd mechanic fields

---
+++ db/migrate/20140211221307_readd_repair_work_classes_to_mechanics.rb
@@ -0,0 +1,6 @@
+class ReaddRepairWorkClassesToMechanics < ActiveRecord::Migration
+  def change
+    add_column :mechanics, :repair_work_classes, :text
+    add_column :mechanics, :tradesperson_certificates, :text
+  end
+end
