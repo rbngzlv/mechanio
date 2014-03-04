87a6eeb33c6cf33f075cabba867c1d0fbe9cbf5e
Boris Bartzitz <bartzitz@gmail.com>
2014-03-04 12:56:35 +0200

    Lock payout form from accidental editing

---
+++ app/assets/javascripts/locked_form.js.coffee
@@ -0,0 +1,11 @@
+jQuery ->
+  $('.locked-form').each (i, form) ->
+    dirty = (input for input in $(form).find('textarea, input:text') when $(input).val() != '')
+    errors = $(form).find('.has-error')
+
+    inputs = $(form).find(':input')
+    inputs.prop('disabled', true) if dirty.length > 0 && errors.length == 0
+
+    $(form).find('.unlock-form').click ->
+      inputs.prop('disabled', false)
+      return false
--- app/controllers/mechanics/payout_methods_controller.rb
+++ app/controllers/mechanics/payout_methods_controller.rb
@@ -1,14 +1,11 @@
 class Mechanics::PayoutMethodsController < Mechanics::ApplicationController
-  def show
-  end
-
   def edit
     current_mechanic.build_associations
   end
 
   def update
     if current_mechanic.update_attributes(permitted_params)
-      redirect_to :back, notice: 'Your payout information successfully updated.'
+      redirect_to edit_mechanics_payout_method_path, notice: 'Your payout information successfully updated.'
     else
       current_mechanic.build_associations
       render :edit
