8e3a929d916927ff126255327a36ce5ac92ae486
Boris Bartzitz <bartzitz@gmail.com>
2014-05-26 13:00:48 +0300

    Add Browse button back to user avatar field

--- app/inputs/image_input.rb
+++ app/inputs/image_input.rb
@@ -2,12 +2,12 @@ class ImageInput < SimpleForm::Inputs::FileInput
   def input
     # options and object exists in parent class
     version     = options.delete(:version) || :thumb
-    image_html  = options.delete(:image_html) || {}
+    browse_html  = options.delete(:browse_html) || {}
 
     image_url   = object.send("#{attribute_name}_url", version)
     file_name   = object.send(attribute_name).file.filename if object.send("#{attribute_name}?") && options.delete(:show_file_name)
 
-    out = template.image_tag(image_url, image_html)
+    out = template.image_tag(image_url)
 
     if file_name
       delete_button = template.content_tag(:span, '×', class: 'btn-change-image btn-delete-image')
@@ -15,6 +15,9 @@ class ImageInput < SimpleForm::Inputs::FileInput
     end
 
     out += @builder.file_field(attribute_name, input_html_options)
+    unless browse_html.empty?
+      out += template.content_tag(:button, 'Browse', browse_html)
+    end
 
     template.content_tag :div, out.html_safe, class: input_html_options.delete(:wrap_class)
   end
