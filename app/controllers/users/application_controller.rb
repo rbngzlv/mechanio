2e41bb2509499561e52440c38aeb12fef0bbf963
Boris Bartzitz <bartzitz@gmail.com>
2014-06-03 16:31:37 +0300

    Refactor email confirmation alert

--- app/controllers/users/application_controller.rb
+++ app/controllers/users/application_controller.rb
@@ -4,20 +4,10 @@ class Users::ApplicationController < ApplicationController
 
   before_filter :store_location, unless: :user_signed_in?
   before_filter :authenticate_user!
-  before_filter :check_email_confirmed
 
   private
 
   def store_location
     session[:previous_url] = request.url if request.get?
-  end
-
-  def check_email_confirmed
-    if current_user && !current_user.confirmed?
-      email = view_context.content_tag :strong, current_user.email
-      url   = user_confirmation_path(user: { email: current_user.email })
-      resend_link = view_context.link_to 'Resend instructions', url, method: :post, class: 'pull-right'
-      flash[:notice] = "Please confirm your email address. A confirmation email was sent to #{email}. #{resend_link}".html_safe
-    end
   end
 end
