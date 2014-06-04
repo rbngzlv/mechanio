f6249836f586c1443fcafa8075ad8ec52758235b
Boris Bartzitz <bartzitz@gmail.com>
2014-06-04 19:39:50 +0300

    Show email confirmation alert on other profile pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -77,15 +77,4 @@ module ApplicationHelper
   def social_connect_button(provider)
     link_to image_tag("#{provider}-btn.jpg"), user_omniauth_authorize_path(provider), class: "#{provider}-link"
   end
-
-  def show_email_confirmation_alert?
-    show = ['users/dashboard', 'users/profiles'].include?(params[:controller])
-    show = show && current_user && !current_user.confirmed?
-  end
-
-  def email_confirmation_alert(email)
-    email_tag = content_tag :strong, email
-    resend_link = link_to 'Resend instructions', user_confirmation_path(user: { email: email }), method: :post, class: 'pull-right'
-    "Please confirm your email address. A confirmation email was sent to #{email_tag}. #{resend_link}".html_safe
-  end
 end
---
+++ app/helpers/email_confirmation_helper.rb
@@ -0,0 +1,13 @@
+module EmailConfirmationHelper
+  def show_email_confirmation_alert?
+    controllers = %w(dashboard cars appointments estimates profiles settings).map { |c| "users/#{c}"}
+    show = controllers.include?(params[:controller])
+    show = show && current_user && !current_user.confirmed?
+  end
+
+  def email_confirmation_alert(email)
+    email_tag = content_tag :strong, email
+    resend_link = link_to 'Resend instructions', user_confirmation_path(user: { email: email }), method: :post, class: 'pull-right'
+    "Please confirm your email address. A confirmation email was sent to #{email_tag}. #{resend_link}".html_safe
+  end
+end
