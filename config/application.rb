397e354d86090235829b82641212333bf8402203
Boris Bartzitz <bartzitz@gmail.com>
2014-07-25 13:02:54 +0300

    Add swf to precompiled assets list

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Mechanio
 
     config.assets.paths << Rails.root.join('vendor', 'assets', 'flash')
 
+    config.assets.precompile += %w(.swf)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
